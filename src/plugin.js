04bb9b5c7109bafe75395e46743a8f8ba970bb3e
mantasandri <mantas.andri@gmail.com>
2022-11-23 17:07:22 -0800

    no-ticket: try to fix ckeditor on vue 3/nuxt

--- src/plugin.js
+++ src/plugin.js
@@ -7,10 +7,8 @@
 import * as Vue from 'vue';
 import CKEditorComponent from './ckeditor.js';
 
-const [ major ] = getVueVersion().split( '.' ).map( i => parseInt( i, 10 ) );
-
 /* istanbul ignore if */
-if ( major < 3 ) {
+if ( Vue.version < 3 ) {
 	throw new Error(
 		'The CKEditor plugin works only with Vue 3+. ' +
 		'For more information, please refer to ' +
@@ -31,19 +29,3 @@ const CKEditor = {
 };
 
 export default CKEditor;
-
-/* istanbul ignore next */
-function getVueVersion() {
-	// Vue 3+.
-	if ( Vue.version ) {
-		return Vue.version;
-	}
-
-	// Webpack complains if the `Vue.default` does not exist. It is exported by Vue 2.
-	// export 'default' (imported as 'Vue') was not found in 'vue'
-	const DEFAULT_KEY = 'default';
-
-	if ( Vue[ DEFAULT_KEY ] ) {
-		return Vue[ DEFAULT_KEY ].version;
-	}
-}
